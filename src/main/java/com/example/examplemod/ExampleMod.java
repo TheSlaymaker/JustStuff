7ea9b983c94a46848a68e5cc5f9dd15d046c6cd3
TheSlaymaker <mrcj987@gmail.com>
2015-12-02 23:24:07 -0600

    yes

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod {
-    public static final String MODID = "HelloWorld";
+    public static final String MODID = "iouAWEBf";
     public static final String VERSION = "1.0";
     
     @EventHandler
